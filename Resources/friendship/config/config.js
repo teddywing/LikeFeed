6425c817f02d7cb99b83f173237ca5d8adaac92a
Teddy Wing <ted@teddywing.com>
2012-07-29 17:47:24 -0400

    In order to FQL likes from our friends, we need to get permission from the current Facebook user to get their friends' likes. There is another permission token called 'user_likes' for the likes of the current user, but I don't think we need that. Now looking into FB permissions, I'm not sure we need 'publish_stream', but we can discuss.

--- Resources/friendship/config/config.js
+++ Resources/friendship/config/config.js
@@ -1,5 +1,5 @@
 (function() {
 	// Facebook
 	Titanium.Facebook.appid = '258369447595838';
-	Titanium.Facebook.permissions = ['publish_stream'];
+	Titanium.Facebook.permissions = ['publish_stream', 'friends_likes'];
 })();
